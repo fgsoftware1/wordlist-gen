f4087bb1a704b52c3d951868d733763c1f6e4f91
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-10-12 18:00:57 +0100

    Update wifiGen.sh

--- wifiGen.sh
+++ wifiGen.sh
@@ -6,4 +6,4 @@ num=0
 read -p "password lenght: " length
 read -p "how many: " num
 
-cat /dev/urandom | tr -dc 'a-z0-9' | fold -w $length | head -n $num
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $length | head -n $num
